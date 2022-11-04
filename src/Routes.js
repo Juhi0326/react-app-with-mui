361c24ee71b9f548d82bd9c3baecb3e5eec6fe8e
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2022-11-04 11:03:20 +0100

    create notFound page

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { useSelector } from 'react-redux';
 
 const AppRoutes = () => {
-
+  const isLogged = useSelector(state => state.user.state);
   return (
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={isLogged ? <NotFound /> : <div>Please log in!</div>} />
     </Routes>
   );
 };
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return (
+    <div>error: 404, Page not found</div>
+  )
+}
+
+export default NotFound;
